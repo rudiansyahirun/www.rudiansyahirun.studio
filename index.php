0ca003396e6a02f881c74e7cf0ed8e643c460b9b
Rudiansyah <rudd.rocker@gmail.com>
2022-08-10 12:55:49 +0800

    add table data

--- index.php
+++ index.php
@@ -7,6 +7,41 @@
     <title>Document</title>
 </head>
 <body>
-    bener ya
+<!--    simple table for
+            Tanggal,
+            Nama Bahan,
+            Harga Bahan,
+            Jumlah Bahan,
+            Harga,
+            Foto Nota,
+            Nama Toko
+        from db 'rumah_reza' and table 'data_material'
+-->
+<table border="1">
+    <tr>
+        <th>Tanggal</th>
+        <th>Nama Bahan</th>
+        <th>Harga Bahan</th>
+        <th>Jumlah Bahan</th>
+        <th>Harga</th>
+        <th>Foto Nota</th>
+        <th>Nama Toko</th>
+    </tr>
+    <?php
+    $db = new PDO('mysql:host=localhost;dbname=rumah_reza', 'root', 'RudiSQLoioi1!');
+    $query = $db->query("SELECT * FROM data_material");
+    while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
+        echo "<tr>";
+        echo "<td>" . $row['tanggal'] . "</td>";
+        echo "<td>" . $row['nama_bahan'] . "</td>";
+        echo "<td>" . $row['harga_bahan'] . "</td>";
+        echo "<td>" . $row['jumlah_bahan'] . "</td>";
+        echo "<td>" . $row['harga'] . "</td>";
+        echo "<td><img src='" . $row['foto_nota'] . "'></td>";
+        echo "<td>" . $row['nama_toko'] . "</td>";
+        echo "</tr>";
+    }
+    ?>
+</table>
 </body>
 </html>
