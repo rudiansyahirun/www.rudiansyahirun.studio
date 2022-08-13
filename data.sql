8915c81bee8cccf85fc5676dcec141af004179ef
rudiansyahirun <rudd.rocker@gmail.com>
2022-08-13 12:25:38 +0800

    set termux

--- data.sql
+++ data.sql
@@ -11,6 +11,6 @@ CREATE TABLE data_material (
                                foto_nota VARCHAR(255) DEFAULT NULL
 );
 
-INSERT INTO data_material (tanggal, nama_bahan, harga_bahan, jumlah_bahan, harga, nama_toko, foto_nota) VALUES ('2020-01-01', 'Bahan 1', '10000', '10', '10000', 'Toko 1', 'foto_nota.jpg');
+INSERT INTO data_material (tanggal, nama_bahan, harga_bahan, jumlah_bahan, harga, nama_toko, foto_nota) VALUES ('2020-01-01', 'Bahan 1', '10000', '10', '100000', 'Toko 1', 'foto_nota.jpg');
 
 UPDATE data_material SET harga = harga_bahan * jumlah_bahan;
--- index.php
+++ index.php
@@ -1,9 +1,33 @@
+<?php
+$mysqli = new mysqli("127.0.0.1","root","1234","rumah_reza");
+
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_erro>
+  exit();
+}
+
+$sql = "SELECT * from data_material";
+$results = $mysqli -> query($sql);
+
+// Associative array
+//$row = $results -> fetch_assoc();
+//printf ("%s (%s)\n", $row["nama_bahan"], $row["harga_bahan"]);
+
+// Free result set
+//$results -> free_result();
+
+//$mysqli -> close();
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="table_style.css">
     <title>Document</title>
 </head>
 <body>
@@ -17,7 +41,7 @@
             Nama Toko
         from db 'rumah_reza' and table 'data_material'
 -->
-<table border="1">
+<table id="table_material">
     <tr>
         <th>Tanggal</th>
         <th>Nama Bahan</th>
@@ -27,22 +51,24 @@
         <th>Foto Nota</th>
         <th>Nama Toko</th>
     </tr>
+    
     <?php
-    $db = new PDO('mysql:host=localhost;dbname=rumah_reza', 'root', '');
-    $query = $db->query("SELECT * FROM data_material");
-    while ($row = $query->fetch(PDO::FETCH_ASSOC)) {
-        echo "<tr>";
-        echo "<td>" . $row['tanggal'] . "</td>";
-        echo "<td>" . $row['nama_bahan'] . "</td>";
-        echo "<td>" . $row['harga_bahan'] . "</td>";
-        echo "<td>" . $row['jumlah_bahan'] . "</td>";
-        echo "<td>" . $row['harga'] . "</td>";
-        echo "<td><img src='" . $row['foto_nota'] . "'></td>";
-        echo "<td>" . $row['nama_toko'] . "</td>";
-        echo "</tr>";
+
+    while ($row = mysqli_fetch_array($results)) {
+      echo "<tr>";
+      echo "<td>" . $row['tanggal'] . "</td>";
+      echo "<td>" . $row['nama_bahan'] . "</td>";
+      echo "<td>" . $row['harga_bahan'] . "</td>";
+      echo "<td>" . $row['jumlah_bahan'] . "</td>";
+      echo "<td>" . $row['harga'] . "</td>";
+      echo "<td><img src='" . $row['foto_nota'] . "'></td>";
+      echo "<td>" . $row['nama_toko'] . "</td>";
+      echo "</tr>";
     }
+    
     ?>
+
 </table>
-<!-- coba remot dari termux -->
+
 </body>
 </html>
